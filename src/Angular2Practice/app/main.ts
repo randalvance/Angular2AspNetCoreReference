89ff789238f2538b7b6394ac050fa75455e3cd58
Randal Vance Cunanan <randalvance@outlook.com>
2016-05-08 11:57:13 +0800

    Used JSPM

---
+++ src/Angular2Practice/app/main.ts
@@ -0,0 +1,7 @@
+﻿/// <reference path="../typings/main.d.ts" />
+import 'reflect-metadata';
+
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app.component';
+
+bootstrap(AppComponent);
