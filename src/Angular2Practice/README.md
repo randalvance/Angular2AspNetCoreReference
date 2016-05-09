bdf39b33f942a4d6ee0569542a5a84c8598fc969
Randal Vance Cunanan <randalvance@outlook.com>
2016-05-09 21:05:24 +0800

    Added README and bower related changes.

---
+++ src/Angular2Practice/README.md
@@ -0,0 +1,14 @@
+﻿# Angular 2 ASP.NET Core Reference Application
+
+This project serves as a personal reference or for others looking on how to setup Angular 2 with ASP.NET Core.
+The IDE used for this project is Visual Studio 2015.
+
+## How to Build
+
+Run the following commands
+```
+npm install
+jspm install
+bower install
+typings install
+```
--- src/Angular2Practice/gulpfile.js
+++ src/Angular2Practice/gulpfile.js
@@ -4,13 +4,12 @@ var runSequence = require("run-sequence");
 
 var Config = new (require("./gulpfile.config"))();
 
-
-gulp.task("clean", function () {
-    return del([Config.paths.dest.app + '/**/*', Config.paths.dest.lib + '/**/*', Config.paths.dest.css + "/**/*"]);
+gulp.task("build", function () {
+    runSequence("clean", ["app", "styles"]);
 });
 
-gulp.task("_default", function () {
-    runSequence("clean", ["app", "styles", "copy-libs"]);
+gulp.task("clean", function () {
+    return del([Config.paths.dest.app + '/**/*', Config.paths.dest.css + "/**/*"]);
 });
 
 gulp.task('app', function () {
@@ -19,12 +18,4 @@ gulp.task('app', function () {
 
 gulp.task('styles', function () {
     gulp.src(Config.paths.src.css + '/**/*', { base: Config.paths.src.css }).pipe(gulp.dest(Config.paths.dest.css));
-});
-
-gulp.task('copy-libs', function () {
-    gulp.src(Config.paths.src.bower + "/jquery/dist/*.js", { base: Config.paths.src.bower + "/jquery/dist" }).pipe(gulp.dest(Config.paths.dest.lib + "/jquery"));
-    gulp.src(Config.paths.src.bower + "/bootstrap/dist/**/*", { base: Config.paths.src.bower + "/bootstrap/dist" }).pipe(gulp.dest(Config.paths.dest.lib + "/bootstrap"));
-    gulp.src(Config.paths.src.bower + "/tether/dist/**/*", { base: Config.paths.src.bower + "/tether/dist" }).pipe(gulp.dest(Config.paths.dest.lib + "/tether"));
-    gulp.src(Config.paths.src.bower + "/es6-shim/*.js", { base: Config.paths.src.bower + "/es6-shim" }).pipe(gulp.dest(Config.paths.dest.lib + "/es6-shim"));
-    gulp.src(Config.paths.src.bower + "/zone/dist/*.js", { base: Config.paths.src.bower + "/zone/dist" }).pipe(gulp.dest(Config.paths.dest.lib + "/zone"));
 });
