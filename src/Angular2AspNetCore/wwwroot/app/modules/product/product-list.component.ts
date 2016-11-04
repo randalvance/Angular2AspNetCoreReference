b01734147382d99c9efd65aa5168df04032c2cc3
Randal Vance Cunanan <randal@randalvance.net>
2016-11-04 11:16:33 +0800

    Used routerLink

--- src/Angular2AspNetCore/wwwroot/app/modules/product/product-list.component.ts
+++ src/Angular2AspNetCore/wwwroot/app/modules/product/product-list.component.ts
@@ -1,5 +1,4 @@
 ﻿import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { Product } from './product';
 import { ProductService } from './product.service';
@@ -13,16 +12,10 @@ export class ProductListComponent implements OnInit {
     products: Observable<Product[]>;
     filterBy: string; 
 
-    constructor(
-        private router: Router,
-        private productService: ProductService) { }
+    constructor(private productService: ProductService) { }
 
     ngOnInit() {
         this.products = this.productService.getProducts();
-    }
-
-    onSelect(product: Product) {
-        this.router.navigate(['/product', product.id]);
     }
 
     onRatingClicked(payload: string) {
